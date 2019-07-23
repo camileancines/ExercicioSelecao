7a7aa92ceebb7ecd98b49d0e21b581a26839cce2
camileancines <camileancines@gmail.com>
2019-07-23 15:54:52 -0300

    criando repositorio

---
+++ ExercicioSelecao/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExercicioSelecao
+//
+//  Created by Camile Ancines on 23/07/19.
+//  Copyright © 2019 Camile Ancines. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
