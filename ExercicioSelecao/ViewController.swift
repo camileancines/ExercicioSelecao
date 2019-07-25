fdfe565aed1f98b7a261ff3838eee0dbf8da9683
camileancines <camileancines@gmail.com>
2019-07-25 11:50:37 -0300

    atualizando repositorio

--- ExercicioSelecao/ViewController.swift
+++ ExercicioSelecao/ViewController.swift
@@ -9,12 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var getTimeButton: UIButton!
+    @IBOutlet weak var goImageView: UIImageView!
+    @IBOutlet weak var lastNumberLabel: UILabel!
+    
+    @IBAction func getTimeButtonAction(_ sender: UIButton) {
+        getTime()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.goImageView.image = UIImage(named: "go4all")
+        self.getTimeButton.layer.cornerRadius = getTimeButton.bounds.height / 2
+        self.getTimeButton.layer.masksToBounds = true
     }
+    
+    func getTime(){
+        
+        let time = Date().currentTimeMillis()
 
-
+        let lastNumber = time % 10
+        
+        if lastNumber == 1 {
+            view.backgroundColor = UIColor(red: 255/255, green: 216/255, blue: 216/255, alpha: 1.0)
+        } else if lastNumber == 2 {
+            view.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 216/255, alpha: 1.0)
+        } else if lastNumber == 3 {
+            view.backgroundColor = UIColor(red: 255/255, green: 243/255, blue: 216/255, alpha: 1.0)
+        } else if lastNumber == 4 {
+            view.backgroundColor = UIColor(red: 239/255, green: 255/255, blue: 216/255, alpha: 1.0)
+        } else if lastNumber == 5 {
+            view.backgroundColor = UIColor(red: 216/255, green: 255/255, blue: 227/255, alpha: 1.0)
+        } else if lastNumber == 6 {
+            view.backgroundColor = UIColor(red: 216/255, green: 255/255, blue: 255/255, alpha: 1.0)
+        } else if lastNumber == 7 {
+            view.backgroundColor = UIColor(red: 216/255, green: 227/255, blue: 255/255, alpha: 1.0)
+        } else if lastNumber == 8 {
+            view.backgroundColor = UIColor(red: 225/255, green: 216/255, blue: 255/255, alpha: 1.0)
+        } else if lastNumber == 9 {
+            view.backgroundColor = UIColor(red: 245/255, green: 216/255, blue: 255/255, alpha: 1.0)
+        } else if lastNumber == 0 {
+            view.backgroundColor = UIColor(red: 237/255, green: 237/255, blue: 237/255, alpha: 1.0)
+        }
+        
+        lastNumberLabel.text = "Last number: \(lastNumber)"
+        
+        print(time)
+    }
 }
 
+extension Date {
+    func currentTimeMillis() -> Int64 {
+        return Int64(self.timeIntervalSince1970 * 1000)
+    }
+}
